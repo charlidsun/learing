3a7f106f5e557162405f6c84878481f120b250d8
charlidsun <yulinsunjingge@gmail.com>
2018-07-06 10:18:49 +0800

    shior练习第一次提交

---
+++ shiroLearning/src/main/java/com/sun/shiroLearning/ShiroLearningApplication.java
@@ -0,0 +1,12 @@
+package com.sun.shiroLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroLearningApplication.class, args);
+	}
+}
