83785c00bee1814c47ab1516b90f9f8dc293eb36
charlidsun <sunjingge@outlook.com>
2018-08-15 16:25:15 +0800

    enterprise web

---
+++ learnVue/src/main/java/com/sun/learnVue/LearnVueApplication.java
@@ -0,0 +1,29 @@
+package com.sun.learnVue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class LearnVueApplication {
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		List<Stu> list = new ArrayList<Stu>();
+		list.add(new Stu("cccc", "dddd"));
+		list.add(new Stu("aaaa", "bbbbb"));
+		model.addAttribute("name", "FootBall");
+		model.addAttribute("players",list);
+		return "index";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnVueApplication.class, args);
+	}
+}
---
+++ learnVue/src/main/java/com/sun/learnVue/Stu.java
@@ -0,0 +1,29 @@
+package com.sun.learnVue;
+
+public class Stu {
+
+	private String description;
+	private String name;
+	
+	public Stu() {
+	}
+	
+	public Stu(String description, String name) {
+		this.description = description;
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
